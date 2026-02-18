23722ba197c605ba60e8d6291b8c0cf8d8cf7f40
tjurjevich <trevor.jurjevich@gmail.com>
2026-02-17 20:48:23 -0600

    Train vae, add params to increase data variability

--- src/training/vae_training.py
+++ src/training/vae_training.py
@@ -11,8 +11,11 @@ NUM_VARS = ["patient_age","patient_height_cm","patient_weight_kg","patient_systo
 CAT_VARS = ["patient_gender","patient_race"]
 
 # Dimensions for inner/outer layers
-EDGE_DIM = 128
-LATENT_DIM = 16
+ENCODER_EDGE_DIM = 256
+DECODER_EDGE_DIM = 128
+LATENT_DIM = 32
+BETA = 8
+FREE_BITS = 2.0
 
 # Encoder framework
 class PatientEncoder(Layer):
@@ -20,7 +23,9 @@ class PatientEncoder(Layer):
         super().__init__()
         # Layers for deterministic portion
         self.dense_1 = Dense(units = edge_dim, activation = "relu")
+        self.batch_norm_1 = BatchNormalization()
         self.dense_2 = Dense(units = edge_dim // 2, activation = "relu")
+        self.batch_norm_2 = BatchNormalization()
         # Layers for reparameterization
         self.z_mean = Dense(units = latent_dim, name = "z_mean")
         self.z_log_var = Dense(units = latent_dim, name = "z_log_var")
@@ -31,26 +36,26 @@ class PatientEncoder(Layer):
     
     def call(self, inputs):
         x = self.dense_1(inputs)
+        x = self.batch_norm_1(x)
         x = self.dense_2(x)
+        x = self.batch_norm_2(x)
         mean = self.z_mean(x)
         log_var = self.z_log_var(x)
         z = self.reparameterization(mean = mean, log_var = log_var)
         return mean, log_var, z
         
-
-
 # Decoder framework
 class PatientDecoder(Layer):
-    def __init__(self, edge_dim: int, latent_dim: int, original_continuous_dimensions, num_categories_list):
+    def __init__(self, edge_dim: int, original_continuous_dimensions, num_categories_list):
         """
         original_continuous_dimensions: normalized, but unchanged continous variables: 6 in this particular use-case
         num_categories_list: cardinality for each discrete variable (order of variables/cardinality will matter): [4, 7] in this particular use-case
         """
         super().__init__()
         # Innermost dense layer
-        self.dense_1 = Dense(units = latent_dim, activation = "relu")
+        self.dense_1 = Dense(units = edge_dim, activation = "relu")
         # Outermost dense layer
-        self.dense_2 = Dense(units = edge_dim, activation = "relu")
+        self.dense_2 = Dense(units = edge_dim * 2, activation = "relu")
 
         # Separate continuous from discrete variables
         self.output_continous = Dense(original_continuous_dimensions)
@@ -63,6 +68,15 @@ class PatientDecoder(Layer):
         x_discrete = [layer(x) for layer in self.output_discrete]
         return tf.concat([x_cont] + x_discrete, axis = -1)
 
+# class KLAnnealingCallback(tf.keras.callbacks.Callback):
+#     def __init__(self, warmup_epochs: int):
+#         super().__init__()
+#         self.warmup_epochs = warmup_epochs
+
+#     def on_epoch_begin(self, epoch, logs=None):
+#         new_weight = min(1.0, (epoch + 1) / self.warmup_epochs)
+#         self.model.kl_weight.assign(new_weight)
+
 # Combined (VAE) framework
 class PatientDataGenerator(tf.keras.Model):
     def __init__(self, encoder, decoder, num_continous_variables):
@@ -70,6 +84,7 @@ class PatientDataGenerator(tf.keras.Model):
         self.encoder = encoder 
         self.decoder = decoder 
         self.num_continuous_variables = num_continous_variables
+        # self.kl_weight = tf.Variable(0.0, trainable=False, name="kl_weight")
     
         self.total_loss_tracker = tf.keras.metrics.Mean(name="total_loss")
         self.reconstruction_loss_tracker = tf.keras.metrics.Mean(name="recon_loss")
@@ -106,10 +121,11 @@ class PatientDataGenerator(tf.keras.Model):
             )
             # 4. KL divergence loss
             kl_loss = -0.5 * (1 + log_var - tf.square(mean) - tf.exp(log_var))
-            kl_loss = tf.reduce_sum(tf.reduce_sum(kl_loss, axis = 1))
+            kl_loss = tf.maximum(kl_loss, FREE_BITS)  # apply free bits per dimension
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
 
             # 5. Add reconstruction losses with KL divergence loss for overall loss
-            total_loss = recon_cat + recon_cont + kl_loss
+            total_loss = recon_cat + recon_cont + (BETA * kl_loss)
             
             # 6. Calculate gradients and apply to current set of model weights
             grads = tape.gradient(total_loss, self.trainable_weights)
@@ -120,13 +136,15 @@ class PatientDataGenerator(tf.keras.Model):
             self.kl_loss_tracker.update_state(kl_loss)
             self.total_loss_tracker.update_state(total_loss)
             return {m.name: m.result() for m in self.metrics}
-        
+
+
+
 # Function which will engineer features from "original" data
 class DataPreprocessor():
     def __init__(self, data: pl.DataFrame):
         self.data_ = data
 
-    def preprocess(self, numeric_feature_names, categorical_feature_names):
+    def transform(self, numeric_feature_names, categorical_feature_names):
         self.numeric_feature_names_ = numeric_feature_names
         self.categorical_feature_names_ = categorical_feature_names
         self.numeric_transformer_ = Pipeline([
@@ -139,12 +157,17 @@ class DataPreprocessor():
             ("numeric", self.numeric_transformer_, numeric_feature_names),
             ("categorical", self.categorical_transformer_, categorical_feature_names)
         ])
-        return self.preprocessor.fit_transform(self.data_.select(numeric_feature_names + categorical_feature_names))
+        # Run fit() method separate from transform() method, which will return the actual values.
+        self.preprocessor.fit(self.data_.select(numeric_feature_names + categorical_feature_names))
+        return self.preprocessor.transform(self.data_.select(numeric_feature_names + categorical_feature_names))
+
+    def inverse_transform(self, raw_data):
+        return self.preprocessor.inverse_transform(raw_data)
 
 if __name__ == "__main__":
     data = pl.read_parquet("./data/original_data.parquet")
     dp = DataPreprocessor(data = data)
-    input_data = dp.preprocess(numeric_feature_names=NUM_VARS, categorical_feature_names=CAT_VARS)
+    input_data = dp.transform(numeric_feature_names=NUM_VARS, categorical_feature_names=CAT_VARS)
 
     # Document field names and possible values, which will need to be used as upcoming parameters for model
     categorical_metadata = {}
@@ -153,9 +176,24 @@ if __name__ == "__main__":
         categorical_metadata[cat] = possible_vals.tolist()
 
     # Initialize encoder & decoder
-    encoder = PatientEncoder(edge_dim=EDGE_DIM, latent_dim=LATENT_DIM)
-    decoder = PatientDecoder(edge_dim=EDGE_DIM, latent_dim=LATENT_DIM, original_continuous_dimensions=len(NUM_VARS), num_categories_list=[len(i) for i in categorical_metadata.values()])
+    encoder = PatientEncoder(edge_dim=ENCODER_EDGE_DIM, latent_dim=LATENT_DIM)
+    decoder = PatientDecoder(edge_dim=DECODER_EDGE_DIM, original_continuous_dimensions=len(NUM_VARS), num_categories_list=[len(i) for i in categorical_metadata.values()])
     
+    # Pass encoder & decoder into PatientDataGenerator object
     vae = PatientDataGenerator(encoder = encoder, decoder = decoder, num_continous_variables = len(NUM_VARS))
 
+    # Compile and train
+    vae.compile(optimizer = "adam")
+    vae.fit(
+        input_data,
+        epochs = 25,
+        batch_size = 256
+    )
+
+    # Sample new points from normal dist (prior)
+    z_new = tf.random.normal(shape = (100, LATENT_DIM))
+
+    # Decode to original input dimensions
+    unprocessed_output = vae.decoder(z_new)
+    # print(dp.preprocessor.named_transformers_["numeric"].inverse_transform(unprocessed_output.numpy()[:, :6]))
 
